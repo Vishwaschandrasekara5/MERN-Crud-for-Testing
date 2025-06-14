5ae88961063bcbe3ab914753bed161c27a5847d4
Vishwaschandrasekara5 <sandunv05@gmail.com>
2025-06-14 12:54:59 +0530

    add the project files controllers, models and routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,18 @@
-console.log("Hii");
+//MeCIZfuUq1Yg1T2d
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middlerware
+
+app.use("/", (req, res, next) => {
+    res.send("Ït is Working");
+})
+
+mongoose.connect("mongodb+srv://admin:MeCIZfuUq1Yg1T2d@cluster0.67s5tt9.mongodb.net/")
+.then(()=>console.log("Connected to MongoDB"))
+.then(()=>{
+    app.listen(5000)
+})
+.catch((err)=> console.log((err)));
