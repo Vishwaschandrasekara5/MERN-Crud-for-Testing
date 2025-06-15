e3e3030a639310b8a10a3c37790d7727a180d15f
Vishwaschandrasekara5 <sandunv05@gmail.com>
2025-06-15 22:02:01 +0530

    update userdetail

--- Frontend/src/components/UserDetails/Userdetail.js
+++ Frontend/src/components/UserDetails/Userdetail.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Nav from '../Navbar/Navbar'
+import axios from 'axios';
+
+const URL = "http://localhost:5000/";
+
+const fetchHandler = async () => {
+    return await axios.get(URL).then((res) => res.data);
+}
 
 function Userdetail() {
+
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        fetchHandler().then((data)=>setUser(data.user))
+    })
   return (
     <div>
-      <h1>This is User Details Page</h1>
+      <Nav/>
     </div>
   )
 }
