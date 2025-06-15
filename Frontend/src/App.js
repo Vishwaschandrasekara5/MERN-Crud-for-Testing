1baada4e222b16010e2ed8192a46766da6b22053
Vishwaschandrasekara5 <sandunv05@gmail.com>
2025-06-15 14:52:51 +0530

    Home page and Navbar updated.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to my react app</h1>
+    <div>
+     <Home/>
     </div>
   );
 }
